ec8a1da8c071a4ad6660de0e7f8c0d74a611f947
Hang ZHONG <38694340+HangZhongZH@users.noreply.github.com>
2019-08-12 23:45:21 +0100

    Add files via upload

---
+++ 198. 打家劫舍.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums == []:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[-1]
